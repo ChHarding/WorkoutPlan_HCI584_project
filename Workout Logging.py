ac285dc7bde801d8d3bcec4a4a23327153d858e5
Abigailmil <am24@iastate.edu>
2024-06-13 14:32:08 -0500

    Implemented a basic progress tracking system

--- Workout Logging.py
+++ Workout Logging.py
@@ -4,6 +4,6 @@ workout_logs = []
 def log_workout(day, exercises_completed):
     workout_logs.append({'day': day, 'exercises': exercises_completed})
 
-log_workout(1, ['Jumping Jacks', 'Knee Push-ups'])
-log_workout(2, ['Squats', 'Knee Push-ups'])
+log_workout(1, ['Jumping Jacks', 'Knee Push-ups']) #user input
+log_workout(2, ['Squats', 'Knee Push-ups']) #user input
 print(workout_logs)
